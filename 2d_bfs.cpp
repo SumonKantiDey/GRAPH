03274e3eb6e10a21efe1461bfad0daee22a5e823
Sumon Kanti Dey <sumonkantidey23@gmail.com>
2018-06-20 17:59:09 +0600

    BFS 2D grid problem

--- 2d_bfs.cpp
+++ 2d_bfs.cpp
@@ -8,10 +8,10 @@ int fx[]={1,-1,0,0};
 int fy[]={0,0,1,-1};
 int row,col,start,enda,finish1,finish2;
 int r,bomb,edge,place;
-bool check(pii p){///check the point is inside the diagram or not
+bool check(pii p){///check the point is inside the board or not
    int x = p.first;
    int y = p.second;
-    if((x>=0 && x<row) && (y>=0 && y<col))
+    if((x >= 0 && x < row) && (y >= 0 && y < col))
     return 1;
     else return 0;
 }
@@ -26,16 +26,16 @@ int bfs(int x,int y)
        temp = Q.front();
        Q.pop();
        if(temp.first == finish1 && temp.second == finish2){
-        return dis[finish1][finish2];///if get the destination
+          return dis[finish1][finish2];// get the destination
        }
        for(int i=0; i<4; i++){
-        int tx = temp.first + fx[i];
-        int ty = temp.second + fy[i];
-        if(check(make_pair(tx,ty))&& adj[tx][ty] == 0){
-            adj[tx][ty] =1;///if i visit the index then filled it by one
-            dis[tx][ty] = dis[temp.first][temp.second] + 1;///find the distance
-            Q.push(make_pair(tx,ty));
-        }
+           int tx = temp.first + fx[i];
+           int ty = temp.second + fy[i];
+           if(check(make_pair(tx,ty))&& adj[tx][ty] == 0){
+               adj[tx][ty] =1;//if i visit the index then filled it by one
+               dis[tx][ty] = dis[temp.first][temp.second] + 1;//find the distance
+               Q.push(make_pair(tx,ty));
+            }
        }
    }
 }
@@ -61,4 +61,3 @@ int main()
     }
   return 0;
 }
-
